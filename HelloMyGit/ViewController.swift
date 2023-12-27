d75fcc89db5d5bd80d3009392270b6531b3c2edb
TCLeeTW <t.c.lee.taiwan@gmail.com>
2023-12-27 20:31:56 +0800

    First Commit

---
+++ HelloMyGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloMyGit
+//
+//  Created by TC Lee on 2023/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
