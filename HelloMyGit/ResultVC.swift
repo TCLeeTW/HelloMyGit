8a6328984f375b9629726c2550454fd9285d447f
Kang <theman19970919@gmail.com>
2023-12-28 22:57:27 +0800

    ResultVC

---
+++ HelloMyGit/ResultVC.swift
@@ -0,0 +1,32 @@
+//
+//  ResultVC.swift
+//  HelloMyGit
+//
+//  Created by 康 on 2023/12/28.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
